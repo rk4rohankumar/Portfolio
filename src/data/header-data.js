9c063019342a81e07c754a7f8a3d6ad7794794a2
Rohan Kumar <96616262+rk4rohankumar@users.noreply.github.com>
2024-10-22 15:56:17 +0530

    projects and profile updated

--- src/data/header-data.js
+++ src/data/header-data.js
@@ -3,7 +3,7 @@ export const headerData = {
   title: "Hello! I am",
   desciption:
     `Hello! I am Rohan a passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving.`,
-  image: "https://i.ibb.co/HtbDHDK/ROHAN.jpg",
+  image: "https://i.ibb.co/fx7ND4y/IMG-20241012-161203.jpg",
   imagebw: "",
   resumePdf:
     "https://drive.google.com/file/d/1MyIaaXakjdTGW7Ur0j8j555hgZ3B7GV6/view?usp=drive_link",
