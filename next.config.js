29b995dc8e081fd19fb567e15ffd6ed9f207714a
Rohan Kumar <96616262+rk4rohankumar@users.noreply.github.com>
2024-10-22 19:11:58 +0530

    ubuntu logo fixed and profile needs to be fixed

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   reactStrictMode: true, // enabled react-strict mode
 
   images: {
-    domains: ["i.ibb.co", "avatars.githubusercontent.com", "ibb.co"],
+    domains: ["i.ibb.co", "avatars.githubusercontent.com", "ibb.co","e7.pngegg.com"],
   },
 };
 
